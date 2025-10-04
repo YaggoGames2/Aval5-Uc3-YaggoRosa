7d219a9d4e4448e202002ed7a236f6f9bd2169e8
unknown <rosayaggo2@gmail.com>
2025-10-04 11:45:25 -0300

    criação da fução de exibir mensagem

--- pages/RealDolarEuroEstruturado.php
+++ pages/RealDolarEuroEstruturado.php
@@ -27,4 +27,8 @@ function converterYuan( $valor, $moeda) {
     $resultado = $valor / $moeda;
     return "R$ $valor = ¥". number_format($resultado, 2,',', '.');
 }
+
+function exibirMensagem($mensagem){
+ return $mensagem;
+}
 ?>
