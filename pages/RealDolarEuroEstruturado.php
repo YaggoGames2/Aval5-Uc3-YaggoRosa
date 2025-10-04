4b9939969a6e95aa85e738cbafa4ccb9971270c1
unknown <rosayaggo2@gmail.com>
2025-10-04 11:21:03 -0300

    criação da função que valida as entradas

--- pages/RealDolarEuroEstruturado.php
+++ pages/RealDolarEuroEstruturado.php
@@ -1,7 +1,16 @@
 <?php
+function validarEntradas($valor, $moeda) {
+    if ($valor === false || $valor <= 0) {
+        return "Valor inválido! Digite um número maior que zero.";
+    }if (empty($moeda)) {
+        return "Selecione uma moeda para conversão.";
+    }
+  $moedasValidas = ["dolar", "euro", "yuan"];
 
-function ValidarEntrada($valor){
- return ((is_numeric($valor)));
+    if (!in_array($moeda, $moedasValidas)) {
+        return "Moeda inválida! Escolha uma opção correta.";
+    }
+return true;
 }
 
 ?>
